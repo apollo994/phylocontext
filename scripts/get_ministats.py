de1cfebf23034c7ab1f9501a255e395f1fb4204c
apollo994 <fabio.zanarello.94@gmail.com>
2025-04-15 17:43:46 +0200

    to extract features

---
+++ scripts/get_ministats.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import argparse
+
+def main():
+
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--input', help='')
+    args = parser.parse_args()
+
+
+
+if __name__ == "__main__":
+    main()
